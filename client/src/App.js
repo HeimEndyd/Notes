94f75eb59b947b862380a2d8b0031a41e688db5a
SuslovMD <maksuslov@gmail.com>
2021-05-04 20:42:55 +0300

    - add some React; - Преднастроен роутинг;

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import 'materialize-css'
+import {BrowserRouter as Router} from 'react-router-dom'
+import { useRoutes } from './routes';
+import 'materialize-css'
+
+function App() {
+  const routes = useRoutes(false);
+  return (
+    <Router>
+    <div className = "container">
+      <h1>
+        {routes}
+      </h1>
+    </div>
+    </Router>
+  )
+}
+
+export default App;
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose')
 const schema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    notes: [{type: Types.ObjectId, ref: 'Note'}]
+    notes: [{type: Schema.Types.ObjectId, ref: 'Note'}]
 })
 
 module.exports = model('User', schema)
