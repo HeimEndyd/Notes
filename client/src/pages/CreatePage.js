c6372f4676a4cc30da6b7afaf6480a42888f0d8a
LENOVO110-SMD\SuslovMD <maksuslov@gmail.com>
2021-08-31 16:07:32 +0300

    - CreateNoteForm + Redux;

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -3,21 +3,10 @@ import { connect } from 'react-redux'
 import { changeText } from '../redux/noteActions'
 
 class CreateNoteForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = { ...props.state }
-  }
-
-  changeInputHandler = (event) => {
-    event.persist()
-    this.props.changeText(event.target.value)
-    this.setState((prev) => ({
-      ...prev,
-      ...{
-        [event.target.name]: event.target.value,
-      },
-    }))
+  changeInputHandler = ({ target }) => {
+    console.log(target.value)
+    this.props.changeText(target.value)
+    console.log(target.value)
   }
 
   render() {
@@ -26,23 +15,22 @@ class CreateNoteForm extends React.Component {
         <div className='col s8 offset-s2'>
           <div className='input-field'>
             <input
-              placeholder='Заметка'
-              id='note'
-              type='text'
               name='note'
-              value={this.state.note}
+              type='text'
+              placeholder='Заметка'
+              value={this.props.newNote.note}
               onChange={this.changeInputHandler}
               // onKeyPress={pressHandler}
             />
             <label htmlFor='note'>Введите текст</label>
             <div>
-              {this.state.tags.map((tag) => {
+              {/* {this.state.tags.map((tag) => {
                 return (
                   <div className={'chip'} key={tag}>
                     {tag}
                   </div>
                 )
-              })}
+              })} */}
             </div>
           </div>
         </div>
@@ -55,9 +43,11 @@ const mapDispatchToProps = {
   changeText,
 }
 
-const mapStateToProps = (state) => ({
-  state: state.newNoteReduser,
-})
+const mapStateToProps = (state) => {
+  return {
+    newNote: state.newNoteReduser,
+  }
+}
 
 export const CreatePage = connect(
   mapStateToProps,
